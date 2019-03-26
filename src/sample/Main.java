26a9c2959822eed6433b47c470bf36bc470d0c14
RobinHood3082 <rkhan3082@gmail.com>
2019-03-26 10:13:09 +0600

    First and Final Commit
    This is the first and final commit of my calendar project.

---
+++ src/sample/Main.java
@@ -0,0 +1,38 @@
+package sample;
+
+import com.sun.javafx.scene.control.skin.DatePickerSkin;
+import javafx.application.Application;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.time.LocalDate;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        try {
+            BorderPane root = new BorderPane();
+            Scene scene = new Scene(root, 400, 220);
+            DatePickerSkin datePickerSkin = new DatePickerSkin(new DatePicker(LocalDate.now()));
+            Node popupContent = datePickerSkin.getPopupContent();
+
+            root.setCenter(popupContent);
+
+            primaryStage.setTitle("Calendar");
+            primaryStage.setResizable(false);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
